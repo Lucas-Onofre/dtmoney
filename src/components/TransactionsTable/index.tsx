088da993a6da402d21a70b80dcf53b151daf3249
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-05 16:06:31 -0300

    Axios configuration

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-    .then(response => response.json())
-    .then(data => setTransactions(data));
+    api.get('transactions')
+    .then(response => console.log(response.data))
   }, [])
 
   return (
