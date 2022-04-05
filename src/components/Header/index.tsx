0e32ca4010f0b45a84e8be53216e3610b310c37a
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-05 01:31:03 -0300

    Component: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Content } from './styles';
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header(){
+  return(
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dtmoney" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+}
