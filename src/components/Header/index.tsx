f9d14695353d31893f0efef51beb83af1319c667
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-07 18:24:25 -0300

    Modal configuration

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,21 @@
-import { Container, Content } from './styles';
-
 import logoImg from '../../assets/logo.svg'
 
-export function Header(){
+import { Container, Content } from './styles';
+
+interface HeaderProps{
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps){
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
         </button>
+
+        
       </Content>
     </Container>
   );
