5cc1c356403ba3aea35f21805c4435257f0e6f85
Lucas-Onofre <lucas_onofre_zte@hotmail.com>
2022-04-08 15:01:51 -0300

    Moving createTransaction to TransactionContext

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,12 +10,21 @@ interface Transaction {
   createdAt: string
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+// type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>
+
 interface TransactionProviderProps{
   children: ReactNode;
 }
 
+interface TransactionsContextData {
+  transactions: Transaction[],
+  createTransaction: (transaction: TransactionInput) => void;
+}
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionProviderProps){
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -23,10 +32,14 @@ export function TransactionsProvider({ children }: TransactionProviderProps){
   useEffect(() => {
     api.get('transactions')
     .then(response => setTransactions(response.data.transactions))
-  }, [])
+  }, []);
+
+  function createTransaction(transaction: TransactionInput){
+    api.post('/transactions', transaction);
+  }
 
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={ { transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   )
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,8 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
-
 import { api } from '../../services/api';
+
+import { TransactionsContext } from '../../TransactionsContext';
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -15,22 +16,22 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState('');
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [transactionType, setTransactionType] = useState('deposit');
 
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
 
-    const newTransaction = { 
-      title, 
-      value, 
-      category, 
-      transactionType
-    };
-
-    api.post('/transactions', newTransaction);
+    createTransaction({
+      title,
+      amount,
+      category,
+      type: transactionType
+    })
   }
 
   return(
@@ -59,8 +60,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={event => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
